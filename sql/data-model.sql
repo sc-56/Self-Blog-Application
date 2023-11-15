733d3327e7392568f787ea122fe7aa78da743d19
sc <yichao.zhang@hotmail.com>
2023-11-15 15:36:55 +0800

    restart

--- sql/data-model.sql
+++
@@ -1,1 +0,0 @@
-CREATE DATABASE q_mgmt;
--- sql/insert.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO questions (title, date_, type_, answer)
-VALUES ('about database', 
-        '2023-10-02', 
-        'technology', 
-        'This is the method to insert something.');
